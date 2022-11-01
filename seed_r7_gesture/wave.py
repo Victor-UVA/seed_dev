960f0318508067a37de34672ae86f96ebf4d7bcb
Andrew Vithoulkas <avithoulkai@gmail.com>
2022-11-01 16:53:59 -0400

    Corrected values, ready to make hand motion

--- seed_r7_gesture/wave.py
+++ seed_r7_gesture/wave.py
@@ -11,9 +11,11 @@ rospy.init_node('move_seed_wave')
 
 robot = moveit_commander.RobotCommander
 rarm_group = moveit_commander.MoveGroupCommander('rarm')
+larm_group = moveit_commander.MoveGroupCommander('larm')
 trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory)
 
 rarm_values = rarm_group.get_current_joint_values()
+larm_values = larm_group.get_current_joint_values()
 
 # Make sure arms are set down
 rarm_values[0] = 0
@@ -23,19 +25,26 @@ rarm_values[3] = 0
 rarm_group.set_joint_value_target(rarm_values)
 rarm_group.set_max_velocity_scaling_factor(0.9)
 
-plan = rarm_group.plan()
-rarm_group.go(wait=True)
+larm_values[0] = 0
+larm_values[1] = 0
+larm_values[2] = 0
+larm_values[3] = 0
+larm_group.set_joint_value_target(larm_values)
+larm_group.set_max_velocity_scaling_factor(0.9)
 
+plan = rarm_group.plan()
+plan2 = larm_group.plan()
+rarm_group.go(wait=True)
+larm_group.go(wait=True)
 
 # Pick arm up and put it in wave location
-rarm_values[0] = -1.75
-rarm_values[1] = -0.35
-rarm_values[2] = -0.4
-rarm_values[3] = 0
-
+rarm_values[0] = -0.35
+rarm_values[1] = -0.4
+rarm_values[2] = 0
+rarm_values[3] = -1.75
 rarm_group.set_joint_value_target(rarm_values)
 
-plan2 = rarm_group.plan()
+plan3 = rarm_group.plan()
 rarm_group.go(wait=True)
 
 
