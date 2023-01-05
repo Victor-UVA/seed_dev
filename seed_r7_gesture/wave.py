a5e85028356395f94b2ab4e37215f6d77d7bbac5
Andrew Vithoulkas <avithoulkai@gmail.com>
2023-01-05 15:53:33 -0500

    Fully completed wave and bow gestures

--- seed_r7_gesture/wave.py
+++ seed_r7_gesture/wave.py
@@ -13,10 +13,12 @@ robot = moveit_commander.RobotCommander
 rarm_group = moveit_commander.MoveGroupCommander('rarm')
 larm_group = moveit_commander.MoveGroupCommander('larm')
 rhand_group = moveit_commander.MoveGroupCommander('rhand')
+lhand_group = moveit_commander.MoveGroupCommander('lhand')
 trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=10)
 
 rarm_values = rarm_group.get_current_joint_values()
 larm_values = larm_group.get_current_joint_values()
+lhand_values = lhand_group.get_current_joint_values()
 rhand_values = rhand_group.get_current_joint_values()
 
 # Make sure arms are set down
@@ -24,7 +26,7 @@ rarm_values[0] = 0
 rarm_values[1] = 0
 rarm_values[2] = 0
 rarm_values[3] = 0
-rarm_values[4] = 0
+rarm_values[7] = 0
 rarm_group.set_joint_value_target(rarm_values)
 rarm_group.set_max_velocity_scaling_factor(1)
 
@@ -32,7 +34,7 @@ larm_values[0] = 0
 larm_values[1] = 0
 larm_values[2] = 0
 larm_values[3] = 0
-larm_values[4] = 0
+larm_values[7] = 0
 larm_group.set_joint_value_target(larm_values)
 larm_group.set_max_velocity_scaling_factor(1)
 
@@ -46,7 +48,7 @@ rarm_values[0] = -1
 rarm_values[1] = -0.11
 rarm_values[2] = 0.228
 rarm_values[3] = -1.83
-rarm_values[4] = 1.5
+rarm_values[7] = 1.5
 rarm_group.set_joint_value_target(rarm_values)
 
 plan3 = rarm_group.plan()
@@ -101,7 +103,7 @@ rarm_values[0] = 0
 rarm_values[1] = 0
 rarm_values[2] = 0
 rarm_values[3] = 0
-rarm_values[4] = 0
+rarm_values[7] = 0
 rarm_group.set_joint_value_target(rarm_values)
 
 plan10 = rarm_group.plan()
